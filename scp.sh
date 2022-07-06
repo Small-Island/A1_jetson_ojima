16429b2bdfc5729349ae807803d1732f67b95efe
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-07-06 15:51:22 +0900

    update

--- scp.sh
+++ scp.sh
@@ -1,2 +1,2 @@
 # !/bin/sh
-scp ./ojima_walk.cpp unitree@192.168.123.161:/home/unitree/unitree_legged_sdk/examples
+scp ./ojima_walk.cpp ./tst.cpp unitree@192.168.123.161:/home/unitree/unitree_legged_sdk/examples
