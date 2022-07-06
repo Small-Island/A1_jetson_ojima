5a6bff6d02ea8659cd34edf5a4b82c96af796a44
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-07-06 17:09:59 +0900

    update

--- ojima_walk.cpp
+++ ojima_walk.cpp
@@ -104,7 +104,9 @@ void Custom::momoUDPRecv() {
                     cmd.pitch = 0;
                     cmd.yaw = 0;
                     (*this).cmd.mode = 1;
-                    robot_control = false;
+                    if (auto_moving_state == 0) {
+                        robot_control = false;
+                    }
                 }
                 else {
                     // (*this).cmd.sideSpeed = 0.5*(int8_t)buf_ptr[1]/127.0;
