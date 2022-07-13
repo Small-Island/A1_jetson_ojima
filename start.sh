7d4a0fc4f1206f06a28d5cd98523bb0593682e9e
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-07-13 18:29:50 +0900

    update

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 cd ~/A1_jetson_ojima
 ./momo_arm64.run
 sleep 20
-~/ojima-librealsense/build/examples/pointcloud/ojima-pointcloud-A1 &
+~/ojima-librealsense/build/examples/pointcloud/ojima-pointcloud_A1 &
 ./udp_client
