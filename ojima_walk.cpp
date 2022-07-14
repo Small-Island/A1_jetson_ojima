7a863ff942171eea3d65955be3f1e349a6197a53
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2022-07-14 11:24:54 +0900

    update

--- ojima_walk.cpp
+++ ojima_walk.cpp
@@ -258,7 +258,7 @@ void Custom::RobotControl()
 
             uint8_t buf_ptr[6] = {0xa5, (uint8_t)auto_moving_state, hx, lx, hz, lz};
             sendto(sockfd, buf_ptr, 6*sizeof(uint8_t), 0, (struct sockaddr *)&addr, sizeof(addr));
-            printf("%d, auto_moving_state %d forwardPosition %.2lf sidePosition %.2lf rotateSpeed %.2lf (deg/sec)\n", robot_control, auto_moving_state, highstate.forwardPosition, highstate.sidePosition, highstate.rotateSpeed);
+            printf("%d, auto_moving_state %d forwardPosition %.2lf sidePosition %.2lf rotateSpeed %.2lf (deg/sec)\n", robot_control, auto_moving_state, highstate.forwardPosition, highstate.sidePosition, highstate.rotateSpeed / M_PI * 180.0);
             show_count = 0;
         }
         show_count++;
