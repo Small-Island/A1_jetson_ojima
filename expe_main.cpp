3966c1e260f53158bde30a0a1a6e0e203526653f
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2023-09-21 13:18:34 +0900

    update

--- expe_main.cpp
+++ expe_main.cpp
@@ -15,41 +15,6 @@
 #include <string>
 #include <sstream>
 
-struct A1Status {
-    float lift;
-    float roll;
-    float pitch;
-    int16_t foot1;
-    int16_t foot2;
-    int16_t foot3;
-    int16_t foot4;
-};
-
-void udp_recv_from_x86(void) {
-    int sockfd_recv = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in addr_recv;
-    addr_recv.sin_family = AF_INET;
-    addr_recv.sin_addr.s_addr = inet_addr("0.0.0.0");
-    addr_recv.sin_port = htons(4123);
-    bind(sockfd_recv, (const struct sockaddr *)&addr_recv, sizeof(addr_recv));
-
-    struct A1Status a1Status;
-    printf("time,lift,roll,pitch,foot1,foot2,foot3,foot4\n");
-    uint32_t time_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    double init_time = time_since_epoch/1000.0;
-    while (1) {
-        int recv_size = recv(sockfd_recv, &a1Status, sizeof(struct A1Status), 0);
-        uint32_t time_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-        double time = time_since_epoch/1000.0 - init_time;
-        // printf(
-        //     "%lf,%lf,%lf,%lf,%d,%d,%d,%d\n",
-        //     time, a1Status.lift, a1Status.roll, a1Status.pitch,
-        //     a1Status.foot1, a1Status.foot2, a1Status.foot3, a1Status.foot4
-        // );
-    };
-}
-
-
 int end = 0;
 
 void cameraSetting() {
@@ -151,6 +116,8 @@ void gst_loopback() {
     }
 }
 
+bool is_gst_record_playing = false;
+
 void gst_record() {
     time_t t = time(NULL);
     struct tm tm;
@@ -169,8 +136,43 @@ void gst_record() {
         tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec
     );
     printf("%s\n", pipe_proc);
+    is_gst_record_playing = true;
     system(pipe_proc);
     end = 1;
+}
+
+struct A1Status {
+    float lift;
+    float roll;
+    float pitch;
+    int16_t foot1;
+    int16_t foot2;
+    int16_t foot3;
+    int16_t foot4;
+};
+
+void udp_recv_from_x86(void) {
+    int sockfd_recv = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in addr_recv;
+    addr_recv.sin_family = AF_INET;
+    addr_recv.sin_addr.s_addr = inet_addr("0.0.0.0");
+    addr_recv.sin_port = htons(4123);
+    bind(sockfd_recv, (const struct sockaddr *)&addr_recv, sizeof(addr_recv));
+
+    struct A1Status a1Status;
+    printf("time,lift,roll,pitch,foot1,foot2,foot3,foot4\n");
+    uint32_t time_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    double init_time = time_since_epoch/1000.0;
+    while (1) {
+        int recv_size = recv(sockfd_recv, &a1Status, sizeof(struct A1Status), 0);
+        uint32_t time_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        double time = time_since_epoch/1000.0 - init_time;
+        // printf(
+        //     "%lf,%lf,%lf,%lf,%d,%d,%d,%d\n",
+        //     time, a1Status.lift, a1Status.roll, a1Status.pitch,
+        //     a1Status.foot1, a1Status.foot2, a1Status.foot3, a1Status.foot4
+        // );
+    };
 }
 
 
